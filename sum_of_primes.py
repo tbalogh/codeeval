e7785557ed40dfdabc4ec6090cb025f548a8e27d
tb <szemjuell@gmail.com>
2016-07-14 16:42:10 +0200

    import only the sqrt from math

--- sum_of_primes.py
+++ sum_of_primes.py
@@ -1,4 +1,4 @@
-import math
+from math import sqrt
 
 
 def is_prime(num):
@@ -8,7 +8,7 @@ def is_prime(num):
     if num == 2:
         return True
 
-    for i in range(2, int(math.sqrt(num)) + 1):
+    for i in range(2, int(sqrt(num)) + 1):
         if num % i == 0:
             return False
 
